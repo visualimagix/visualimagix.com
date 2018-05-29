85266e9c31280bfed373208f898df2a90ae94c9b
therealvasanth <therealvasanth@gmail.com>
2018-05-29 13:46:56 +0530

    changing phone number and adding restoration section to portfolio

---
+++ docs/js/main.js
@@ -0,0 +1,13 @@
+    $(document).ready(function(){
+      $('.grass-images, .sky-images, .object-images, .twilight-images, .restored-images').slick({
+				autoplay: true,
+				autoplaySpeed: 1800,
+				arrows : false,
+				fade : true,
+				centerMode : true,
+				speed : 600,
+				infinite : true,
+				cssEase: 'linear',
+			});				
+    });
+
